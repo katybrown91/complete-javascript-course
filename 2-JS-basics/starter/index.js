43b80e85dcb5c873d8c219ca650511c5d74eb9a9
Katy Brown <thisis.katybrown@gmail.com>
2019-07-07 16:22:07 -0400

    challenge

--- 2-JS-basics/starter/index.js
+++ 2-JS-basics/starter/index.js
@@ -29,6 +29,35 @@ isMarried = false;
 
 console.log(firstName + 'is a ' + age + 'year old ' + job + '. Is he married? ' + isMarried);
 
-alert(firstName + 'is a ' + age + 'year old ' + job + '. Is he married? ' + isMarried);
+//alert(firstName + 'is a ' + age + 'year old ' + job + '. Is he married? ' + isMarried);
 
 //prompt user add info
+
+//Basic Operators
+
+var yearJohn = 2018 - 28;
+console.log(yearJohn);
+
+//Operator Precedence
+
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
+
+var isFullAge = now - yearJohn >= fullAge; //true
+console.log(isFullAge);
+
+//Challenge
+
+// BMI = mass / height^2 = mass / (height * height)
+
+var massJohn = 160;
+var heightJohn = 75;
+var massMark = 200;
+var heightMark = 75;
+ 
+var bmiJohn = massJohn / (heightJohn * heightJohn)
+var bmiMark = massMark / (heightMark * heightMark)
+
+var isHigher = bmiMark > bmiJohn;
+console.log('Is Mark\'s BMI higher than John\'s?' + isHigher)

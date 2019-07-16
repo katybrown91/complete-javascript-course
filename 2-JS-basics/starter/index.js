12030f01a03c5dd2296b6f8ece364a3cb2a817d4
Katy Brown <thisis.katybrown@gmail.com>
2019-07-16 15:55:58 -0400

    functions

--- 2-JS-basics/starter/index.js
+++ 2-JS-basics/starter/index.js
@@ -106,3 +106,12 @@ if(height){
 } else{
   console.log('The variable has not been defined');
 }
+
+//Functions
+
+function calculateAge(birthYear){
+  return 2019-birthYear;
+}
+
+var ageJohn =calculateAge(1990);
+console.log(ageJohn);
